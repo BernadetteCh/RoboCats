c6687051008238da5323bea3bd96cfd39f3dbeff
Bernadette Christiner <b.christiner93@gmail.com>
2023-04-27 11:34:07 +0200

    initial commit

---
+++ src/main/java/com/robocat/roboCat/RoboCatApplication.java
@@ -0,0 +1,13 @@
+package com.robocat.roboCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robocat/roboCat/RoboCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robocat.roboCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
