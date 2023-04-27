a71e7c5d2b111ee762577b85dd82b255649f42a9
Bernadette Christiner <b.christiner93@gmail.com>
2023-04-27 14:55:11 +0200

    create random Cat

---
+++ src/main/java/com/robocat/roboCat/service/CatCreator.java
@@ -0,0 +1,30 @@
+package com.robocat.roboCat.service;
+
+import com.robocat.roboCat.model.BreedType;
+import com.robocat.roboCat.model.Cat;
+
+import java.util.Random;
+
+public class CatCreator {
+    private final RandomProvider randomProvider;
+
+    public CatCreator(RandomProvider randomProvider) {
+        this.randomProvider = randomProvider;
+    }
+
+    public Cat createRandomCat() {
+        return new Cat(randomProvider.createRandomIndex(20), generateRandomName(), generateRandomBreed());
+    }
+
+    private String generateRandomName() {
+        String[] catNames = {"Lacky", "Speedy", "Whiskers", "Mufasa", "Fluffy"};
+        int randomIndex = randomProvider.createRandomIndex(catNames.length);
+        return catNames[randomIndex];
+    }
+
+    private BreedType generateRandomBreed() {
+        BreedType breedTypeEnum[] = BreedType.values();
+        int randomIndex = randomProvider.createRandomIndex(breedTypeEnum.length);
+        return breedTypeEnum[randomIndex];
+    }
+}
