777a289e752d748775158d983d784ca5973102c0
Bernadette Christiner <b.christiner93@gmail.com>
2023-04-27 17:09:23 +0200

    create get, post, delete, patch requests

--- src/main/java/com/robocat/roboCat/endpoint/CatEndpoint.java
+++ src/main/java/com/robocat/roboCat/endpoint/CatEndpoint.java
@@ -1,2 +1,50 @@
-package com.robocat.roboCat.endpoint;public class CatEndpoint {
+package com.robocat.roboCat.endpoint;
+
+import com.robocat.roboCat.model.Cat;
+import com.robocat.roboCat.service.CatCreator;
+import com.robocat.roboCat.service.CatStorage;
+import com.robocat.roboCat.service.RandomProvider;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("cats")
+public class CatEndpoint {
+    private final CatStorage catStorage;
+
+    public CatEndpoint(CatStorage catStorage) {
+        this.catStorage = catStorage;
+    }
+
+    @PostMapping
+    public Cat create(@RequestBody Cat cat) {
+        catStorage.create(cat);
+        return cat;
+    }
+
+    @GetMapping
+    public List<Cat> readAll() {
+        return catStorage.readAll();
+    }
+
+    @GetMapping("/randomCats")
+    public List<Cat> readRandomGeneratedCat() {
+        RandomProvider randomProvider = new RandomProvider();
+        CatCreator catCreator = new CatCreator(randomProvider);
+        catStorage.addRandomlyCreatedCatToList(catCreator);
+        return catStorage.readAllRandomGeneratedCats();
+    }
+
+    @PatchMapping
+    public Cat update(@RequestBody Cat cat) {
+        return catStorage.update(cat);
+    }
+
+    @DeleteMapping("{name}")
+    public void delete(@PathVariable String name) {
+        catStorage.delete(name);
+    }
+
 }
