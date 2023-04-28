8ce990ad0720c716523270859696536637ffb7bd
Bernadette Christiner <b.christiner93@gmail.com>
2023-04-28 10:43:09 +0200

    add excpetion for requested cat

---
+++ src/main/java/com/robocat/roboCat/endpoint/CatNotFoundException.java
@@ -0,0 +1,9 @@
+package com.robocat.roboCat.endpoint;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CatNotFoundException extends Exception {
+
+}
